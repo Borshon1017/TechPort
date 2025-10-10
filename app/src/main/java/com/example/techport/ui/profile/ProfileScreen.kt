c7b3ddab3e04424bd8a6ce2a1e2b1751da8e01fe
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2025-10-10 12:05:45 +0200

    profile

--- app/src/main/java/com/example/techport/ui/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/techport/ui/profile/ProfileScreen.kt
@@ -1,7 +1,8 @@
 package com.example.techport.ui.profile
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,16 +12,22 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.techport.ui.theme.TechPOrtTheme
@@ -30,17 +37,39 @@ fun ProfileScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        ProfileOption(text = "Change Profile Picture", icon = Icons.Default.AccountCircle)
+        Box {
+            Icon(
+                imageVector = Icons.Default.AccountCircle,
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .size(128.dp)
+                    .clip(CircleShape),
+            )
+            IconButton(
+                onClick = { /* Handle edit profile picture */ },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .background(MaterialTheme.colorScheme.surface, CircleShape)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Profile Picture",
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(32.dp))
         ProfileOption(text = "Edit Profile", icon = Icons.Default.Edit)
         ProfileOption(text = "Change Password", icon = Icons.Default.Lock)
         ProfileOption(text = "About Us", icon = Icons.Default.Info)
+        ProfileOption(text = "Logout", icon = Icons.Default.ExitToApp)
     }
 }
 
 @Composable
-fun ProfileOption(text: String, icon: androidx.compose.ui.graphics.vector.ImageVector) {
+fun ProfileOption(text: String, icon: ImageVector) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
