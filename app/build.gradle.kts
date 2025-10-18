7783af5ffd684c09db261f0d4f8b9658b86ddd48
Borshon Alfred Gomes <87852753+Borshon1017@users.noreply.github.com>
2025-10-18 14:06:42 +0200

    Revert "login overhawl"
    This reverts commit eba6b3875dca560d814eb173bc688969c86fd237.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.material:material-icons-extended:1.6.8")
 
     implementation("androidx.navigation:navigation-compose:2.8.0")
 
